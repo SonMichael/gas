2e48113d157363807fbd4ecca6c6dca8df6c7228
sonle <sonle@les-MacBook-Pro.local>
2021-03-16 09:22:45 +0700

    welcome

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
+    <title>Welcome</title>
+</head>
+<body style="margin:10%;background-image: url('images/bg.jpg');">
+<div class="container">
+    <div class="row">
+        <div class="panel panel-success">
+            <div class="panel-heading" style="text-align: center">
+                <h3 class="panel-title">Welcome</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
